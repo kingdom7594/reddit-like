d881dec23949276d735624936fde66481f74de55
king <kingdom7594@gmail.com>
2017-06-23 14:43:34 +0800

    start project to establish to a website like reddit !

---
+++ src/main/java/com/redditlike/redditor/RedditLikeApplication.java
@@ -0,0 +1,12 @@
+package com.redditlike.redditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditLikeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/redditlike/redditor/RedditLikeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redditlike.redditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedditLikeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
