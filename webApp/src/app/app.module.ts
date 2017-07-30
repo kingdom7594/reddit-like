3320b4dd7ccb44ef9670fd499ee01158a8405957
Administrator <Administrator@PC-20170704XWLZ>
2017-07-30 14:16:48 +0800

    SAP首页设计

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MaterialModule} from '@angular/material';
+import { NgModule } from '@angular/core'; 
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MaterialModule
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
